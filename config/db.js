49d6102492f9691dbbdeee9ef96b70d88276f997
hunterhr58063 <himanshugarg58063@gmail.com>
2025-02-21 22:55:54 +0530

    Updat Readme File

--- config/db.js
+++ config/db.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false);
-        console.log(process.env.MONGO_URI)
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
